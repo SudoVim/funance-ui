6c350e3899a2c688e3f8e0987b3ac901c3d79ccd
Michael Renken <michaelarenken@gmail.com>
2022-08-20 13:14:48 -0500

    Added a header bar

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,35 @@ function App() {
         width: "100%",
         background: theme.palette.background.default,
       }}
-    ></Box>
+    >
+      <Box
+        sx={{
+          width: "100%",
+          height: "30px",
+          padding: "5px",
+          alignItems: "center",
+          justifyContent: "space-between",
+          background: theme.palette.primary.main,
+          display: "flex",
+          direction: "row",
+        }}
+      >
+        <Box>
+          <Box
+            sx={{
+              fontSize: "large",
+              fontWeight: "bold",
+              color: theme.palette.text.primary,
+            }}
+          >
+            Funance
+          </Box>
+        </Box>
+        <Box>
+          <Box sx={{ color: theme.palette.text.primary }}>login</Box>
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
