ea3034b19a7697cabff0fb22799dcdfc793202b2
Michael Renken <michaelarenken@gmail.com>
2023-01-07 11:42:24 -0600

    Fix some .env issues

--- src/features/api/request.ts
+++ src/features/api/request.ts
@@ -1,8 +1,10 @@
 import { call } from "redux-saga/effects";
 
-export const API_URL = process.env.API_URL ?? "http://localhost:8005/api/v1/";
+export const API_URL =
+  process.env.REACT_APP_API_URL ?? "http://localhost:8005/api/v1/";
 export const API_CROSS_SITE =
-  process.env.NODE_ENV !== "test" && process.env.API_CROSS_SITE === "true";
+  process.env.NODE_ENV !== "test" &&
+  process.env.REACT_APP_API_CROSS_SITE === "true";
 
 function pathToURL(path: string): string {
   if (path.startsWith(API_URL)) {
