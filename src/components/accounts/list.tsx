361a180f2005c39e2e00c7e8074d64a462aa5abc
Michael Renken <michaelarenken@gmail.com>
2023-09-28 08:54:05 -0500

    Added menu to the accounts list page

--- src/components/accounts/list.tsx
+++ src/components/accounts/list.tsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useCallback, useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { actions, selectors } from "features";
-import { PaginatedTable } from "components/utils";
+import { PaginatedTable, SimpleMenu } from "components/utils";
 import { PaginatedEndpointRequest } from "features/api/endpoint";
 import { HoldingAccount } from "features/holdings/types";
 import { endpoints } from "features";
+import { Box } from "@mui/material";
 
 export type Props = {
   page?: number;
 };
+
+const MENU_ITEMS = [
+  {
+    key: "create",
+    label: "Create Account",
+    link: "/app/accounts/create",
+  },
+];
 
 const HEADERS = [
   {
@@ -47,12 +56,21 @@ export const List: React.FC<Props> = ({ page }: Props) => {
   );
 
   return (
-    <PaginatedTable<PaginatedEndpointRequest, HoldingAccount>
-      page={page}
-      slice={endpoints.holdings.accounts.list}
-      endpoint={accountsEndpoint}
-      headers={HEADERS}
-      getRow={getRow}
-    />
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        rowGap: 2,
+      }}
+    >
+      <SimpleMenu items={MENU_ITEMS} />
+      <PaginatedTable<PaginatedEndpointRequest, HoldingAccount>
+        page={page}
+        slice={endpoints.holdings.accounts.list}
+        endpoint={accountsEndpoint}
+        headers={HEADERS}
+        getRow={getRow}
+      />
+    </Box>
   );
 };
