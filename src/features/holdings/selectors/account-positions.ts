3f951dc8243ceb11016e8d6ab50de0ca0cacbe60
Michael Renken <michaelarenken@gmail.com>
2023-09-28 08:47:19 -0500

    Fix build

--- src/features/holdings/selectors/account-positions.ts
+++ src/features/holdings/selectors/account-positions.ts
@@ -27,7 +27,7 @@ export const getPositions = createSelector(
 
         let remaining = -1 * purchase.quantity;
         let head = 0;
-        const update = {};
+        const update: any = {};
         for (const heldPurchase of heldPurchases) {
           if (heldPurchase.quantity <= remaining) {
             remaining -= heldPurchase.quantity;
